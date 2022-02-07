9541a5386710f0aa68258a1eef5cbb8e2a7ca1dd
Mustakmia <musyafijur52@gmail.com>
2022-02-07 18:33:05 +0600

    first commit

---
+++ src/Components/Resume.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import { InnerLayout } from '../styles/Layouts';
+import Title from '../Components/Title';
+import SmallTitle from '../Components/SmallTitle';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SchoolIcon from '@material-ui/icons/School';
+import ResumeItem from '../Components/ResumeItem';
+
+function Resume() {
+    const briefcase = <BusinessCenterIcon />
+    const school = <SchoolIcon />
+    return (
+        <ResumeStyled>
+            <Title title={'Resume'} span={'resume'} />
+            <InnerLayout>
+                <div className="small-title">
+                    <SmallTitle icon={briefcase} title={'PROJECT Experience'} />
+                </div>
+                <div className="resume-content">
+                    <ResumeItem
+                        year={'2021 - 2022'}
+                        title={'Web Development Course'}
+                        subTitle={'Programming Hero'}
+                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '}
+                    />
+                    <ResumeItem
+                        year={'2021 - 2022'}
+                        title={'Hacker Rank'}
+                        subTitle={'30 Days Javascript Challenge'}
+                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '}
+                    />
+                    <ResumeItem
+                        year={'2021 - Present'}
+                        title={'Learn With '}
+                        subTitle={'Redux,React Native'}
+                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '}
+                    />
+                </div>
+                <div className="small-title u-small-title-margin">
+                    <SmallTitle icon={school} title={'Educational Qualifications'} />
+                </div>
+                <div className="resume-content ">
+                    <ResumeItem
+                        year={'2007 - 2009'}
+                        title={'Higher Secondary'}
+                        subTitle={'Netrakona Gove:ment Collage'}
+                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '}
+                    />
+                    <ResumeItem
+                        year={'2019 - Present'}
+                        title={'English Literature'}
+                        subTitle={'City Univercity'}
+                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '}
+                    />
+                    <ResumeItem
+                        year={'2021 - Present'}
+                        title={'Web Development Course'}
+                        subTitle={'Programming Hero'}
+                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '}
+                    />
+                </div>
+            </InnerLayout>
+        </ResumeStyled>
+    )
+}
+
+const ResumeStyled = styled.section`
+    .small-title{
+        padding-bottom: 3rem;
+    }
+    .u-small-title-margin{
+        margin-top: 4rem;
+    }
+
+    .resume-content{
+        border-left: 2px solid var(--border-color);
+    }
+`;
+export default Resume
